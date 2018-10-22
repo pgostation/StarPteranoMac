2c92528146c78db30defa4252e5c5a3516e76deb
pgo <waffle@tg7.so-net.ne.jp>
2018-10-22 23:57:26 +0900

    Initial Commit

---
+++ StarPteranoMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  StarPteranoMac
+//
+//  Created by takayoshi on 2018/10/22.
+//  Copyright © 2018 pgostation. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
