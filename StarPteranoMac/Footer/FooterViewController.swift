e5d42f3bc70e8ce3d9e6d0103632f20fd68e51d3
pgostation <waffle@tg7.so-net.ne.jp>
2019-03-16 11:31:50 +0900

    アプリアイコンを追加

---
+++ StarPteranoMac/Footer/FooterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FooterViewController.swift
+//  StarPteranoMac
+//
+//  Created by takayoshi on 2019/03/15.
+//  Copyright © 2019 pgostation. All rights reserved.
+//
+
+import Cocoa
+
+final class FooterViewController: NSViewController {
+    
+}
